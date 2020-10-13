7708a2256051852306bb2f87283a482bbe1e8571
kevinchfe <546598819@qq.com>
2020-10-13 15:48:08 +0800

    路由注册

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/kevinchfe/blog-service
 go 1.15
 
 require (
-	github.com/gin-gonic/gin v1.6.0 // indirect
+	github.com/gin-gonic/gin v1.6.0
 	github.com/go-playground/validator/v10 v10.4.0 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
---
+++ internal/routers/router.go
@@ -0,0 +1,33 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	v1 "github.com/kevinchfe/blog-service/internal/routers/api/v1"
+)
+
+func NewRouter() *gin.Engine {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	article := v1.Article{}
+	tag := v1.Tag{}
+
+	apiv1 := r.Group("/api/v1")
+	{
+		apiv1.POST("/tags", tag.Create)
+		apiv1.DELETE("tags/:id", tag.Delete)
+		apiv1.PUT("/tags/:id", tag.Update)
+		apiv1.PATCH("/tags/:id/state", tag.Update)
+		apiv1.GET("/tags", article.Get)
+		apiv1.GET("/tags:/id", tag.List)
+
+		apiv1.POST("/articles", article.Create)
+		apiv1.DELETE("/article/:id", article.Delete)
+		apiv1.PUT("/article/:id", article.Update)
+		apiv1.PATCH("/article/:id/state", article.Update)
+		apiv1.GET("/articles", article.List)
+		apiv1.GET("/articles/:id", article.Get)
+	}
+	return r
+}
