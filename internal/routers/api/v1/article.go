bd0f388e2f9e29cd05f8cae5749b5f3ed3149e40
kevinchfe <546598819@qq.com>
2020-10-14 13:09:15 +0800

    引入viper配置

--- internal/routers/api/v1/article.go
+++ internal/routers/api/v1/article.go
@@ -9,22 +9,22 @@ func NewArticle() Article {
 	return Article{}
 }
 
-func (a Article) Get(c *gin.Context) {
+func (a *Article) Get(c *gin.Context) {
 
 }
 
-func (a Article) List(c *gin.Context) {
+func (a *Article) List(c *gin.Context) {
 
 }
 
-func (a Article) Create(c *gin.Context) {
+func (a *Article) Create(c *gin.Context) {
 
 }
 
-func (a Article) Update(c *gin.Context) {
+func (a *Article) Update(c *gin.Context) {
 
 }
 
-func (a Article) Delete(c *gin.Context) {
+func (a *Article) Delete(c *gin.Context) {
 
 }
--- main.go
+++ main.go
@@ -9,11 +9,18 @@ import (
 func main() {
 	router := routers.NewRouter()
 	s := &http.Server{
-		Addr: ":8080",
-		Handler: router,
-		ReadTimeout: 10*time.Second,
-		WriteTimeout: 10*time.Second,
-		MaxHeaderBytes: 1<<20,
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
 	}
 	s.ListenAndServe()
+	//r := gin.Default()
+	//r.GET("/ping", func(c *gin.Context) {
+	//	c.JSON(200, gin.H{
+	//		"message": "pong",
+	//	})
+	//})
+	//r.Run()
 }
