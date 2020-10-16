4cd65d5d4df9307503ac063627a946a6665f1d63
kevinchfe <546598819@qq.com>
2020-10-16 14:51:15 +0800

    注册回调

--- internal/routers/api/v1/tag.go
+++ internal/routers/api/v1/tag.go
@@ -36,7 +36,7 @@ func (t *Tag) Get(c *gin.Context) {
 func (t *Tag) List(c *gin.Context) {
 	param := struct {
 		Name  string `form:"name" binding:"max=100"`
-		State uint8  `form:"state,default=1" binding:"oneof= 0 1"`
+		State uint8  `form:"state,default=1" binding:"oneof=0 1"`
 		ID uint32  `form:"id,default=1" binding:"gte=2"`
 	}{}
 	response := app.NewResponse(c)
